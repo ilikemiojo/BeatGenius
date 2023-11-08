8625b941b94d5b9e380a0ef60c736825a1253c23
ilikemiojo <anwoiciruam@gmail.com>
2023-11-08 11:05:31 -0300

    Revert "Merge branch 'main' into mauricio"
    This reverts commit c2ce60e7d7a3fdf81ada9243738b8e29a378173d, reversing
    changes made to 7dedb1bb14238abe82a82914e9c1402021cee378.

--- Project/scripts/game.gd
+++ Project/scripts/game.gd
@@ -3,24 +3,21 @@ extends Node2D
 var score = 0
 @export var hitPoints = 10
 var comboMultiplier = 1
+var hit = false
 var bumboHit = false
 var caixaHit = false
 var beat_atual = 0
 var posicaoBumbo = 0
 var posicaoCaixa = 0
-
-@export var baiao_01_bumbo = [12.0, 12.75, 14.0, 14.75, 16.0, 28.0, 28.75, 30.0, 30.75, 32.0, 44.0, 44.75, 45.75, 46.0, 46.75, 47.5, 48.0]
-@export var baiao_01_caixa = [13.5, 15.5, 28.5, 29.5, 30.5, 31.5, 44.5, 45.5, 46.5, 47.25]
-#@export var baiao_02_bumbo = [29.0, 29.75, 31.0, 31.75, 33.0]
-#@export var baiao_02_caixa = [29.5, 30.5, 31.5, 32.5]
-#@export var baiao_03_bumbo = [44.0, 44.75, 45.75, 46.0, 46.75, 47.5, 48.0]
-#@export var baiao_03_caixa = [44.5, 45.5, 46.5, 47.25]
+@export var baiao_01_bumbo = []
+@export var baiao_01_caixa = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Nota.set_process_mode(PROCESS_MODE_DISABLED)
 	$Nota.visible = false
 	$HUD.update_score(score)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func nota_process_mode(status, tipo):
@@ -84,48 +81,10 @@ func _input(event): #Tocar com as Setas do Teclado
 		if event.pressed and event.keycode == KEY_RIGHT:
 			_on_bumbo_pressed() # (->) = Bumbo
 
+
 func _on_conductor_beat(beat):
 	beat_atual = beat
-	print(beat)
-	print(posicaoBumbo)
-	print(posicaoCaixa)
 
 func update_score():
 	score += hitPoints * comboMultiplier
 	$HUD.update_score(score)
-
-func _on_conductor_baiao_01_finished():
-	if(score < 70):
-		print('você falhou')
-		posicaoBumbo = 0
-		posicaoCaixa = 0
-		score = 0
-		$HUD.update_score(0)
-		$Conductor.restart(0.0, 0)
-	else:
-		print('você passou da primeira fase')
-		
-
-
-func _on_conductor_baiao_02_finished():
-	if(score < 160):
-		print('você falhou')
-		posicaoBumbo = 5
-		posicaoCaixa = 2
-		score = 70
-		$HUD.update_score(70)
-		$Conductor.restart(11.34, 17)
-	else:
-		print('você passou da segunda fase')
-
-
-func _on_conductor_baiao_03_finished():
-	if(score < 290):
-		print('você falhou')
-		posicaoBumbo = 10
-		posicaoCaixa = 6
-		score = 180
-		$HUD.update_score(180)
-		$Conductor.restart(22.02, 33)
-	else:
-		print('você passou da terceira fase')
